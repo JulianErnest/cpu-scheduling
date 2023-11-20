175c1b373fee05c29fc08b3f49dee4def8184cf9
Julian Ernest <julian@old.st>
2023-11-21 02:39:02 +0800

    fix: adjusted sjf logic

--- src/algorithms/sjf.ts
+++ src/algorithms/sjf.ts
@@ -1,15 +1,19 @@
-import { AlgorithmResultData, ChartData, TableData } from "../types";
+import { AlgorithmResult, AlgorithmResultData, ChartData, TableData } from "../types";
 
 const sjfAlgorithm = (tableData: TableData[]): AlgorithmResultData => {
   // Sort the processes based on arrival time and burst time
-  const sortedTableData = [...tableData].sort(
+  const algorithmResult: AlgorithmResult[] = tableData.map((item) => 
+  ({  ...item, endTime: 0, turnaroundTime: 0, waitingTime: 0 }));
+  const sortedTableData = tableData.sort(
     (a, b) => +a.arrivalTime - +b.arrivalTime || +a.burstTime - +b.burstTime
   );
+
+  
+
+
   const chartData: ChartData[] = [];
 
   let currentTime = 0;
-  let totalTurnaroundTime = 0;
-  let totalWaitingTime = 0;
 
   if (currentTime !== +sortedTableData[0].arrivalTime) {
     chartData.push({
@@ -20,42 +24,53 @@ const sjfAlgorithm = (tableData: TableData[]): AlgorithmResultData => {
     currentTime = +sortedTableData[0].arrivalTime;
   }
 
-  const sjfResult = sortedTableData.map((process) => {
-    if (currentTime < +process.arrivalTime) {
-      currentTime = +process.arrivalTime;
+
+
+  console.log(JSON.stringify(sortedTableData[0]))
+  currentTime += sortedTableData[0].burstTime
+  chartData.push({
+    start: currentTime - sortedTableData[0].burstTime,
+    end: currentTime,
+    id: sortedTableData[0].id,
+  });
+  sortedTableData[0].burstTime = 0;
+
+  let allBurst = sortedTableData.reduce((acc, curr) => +acc + +curr.burstTime, 0);
+  const readyQueue: TableData[] = [];
+  readyQueue.push(sortedTableData[0]);
+  while (allBurst > 0) {
+    console.log(currentTime)
+    for (let i = 0; i <sortedTableData.length; i++) {
+      const index = readyQueue.findIndex((val) => val.id === sortedTableData[i].id);
+      console.log(index)
+      if (sortedTableData[i].burstTime != 0 && +sortedTableData[i].arrivalTime <= currentTime && index === -1) {
+        readyQueue.push(sortedTableData[i]);
+      }
     }
-
-    const waitingTime = currentTime - +process.arrivalTime;
-    const endTime = currentTime + +process.burstTime;
-    const turnaroundTime = endTime - +process.arrivalTime;
-
+    readyQueue.sort((a, b) => +a.burstTime - +b.burstTime);
+    readyQueue.shift();
+    const index = sortedTableData.findIndex((item) => item.id === readyQueue[0]?.id);
     chartData.push({
       start: currentTime,
-      end: endTime,
-      id: process.id,
-    });
+      end: currentTime + sortedTableData[index].burstTime,
+      id: tableData[index].id,
+    })
+    currentTime += readyQueue[0].burstTime;
+    sortedTableData[index].burstTime = 0;
 
-    totalTurnaroundTime += turnaroundTime;
-    totalWaitingTime += waitingTime;
+    algorithmResult[index].endTime = currentTime;
+    algorithmResult[index].turnaroundTime = currentTime - +tableData[index]?.arrivalTime;
+    algorithmResult[index].waitingTime = algorithmResult[index].turnaroundTime - +algorithmResult[index]?.burstTime;
+ 
+    allBurst = sortedTableData.reduce((acc, curr) => acc + +curr.burstTime, 0);
+  }
 
-    currentTime = endTime;
-
-    return {
-      ...process,
-      endTime,
-      turnaroundTime,
-      waitingTime,
-    };
-  });
-
-  const averageTurnaroundTime = totalTurnaroundTime / tableData.length;
-  const averageWaitingTime = totalWaitingTime / tableData.length;
-
+  
   return {
-    algorithmResult: sjfResult,
+    algorithmResult: algorithmResult,
     averageTime: {
-      turnaroundTime: averageTurnaroundTime,
-      waitingTime: averageWaitingTime,
+      turnaroundTime: algorithmResult.reduce((acc, curr) => acc + curr.turnaroundTime, 0) / algorithmResult.length,
+      waitingTime: algorithmResult.reduce((acc, curr) => acc + curr.waitingTime, 0) / algorithmResult.length,
     },
     ganttChartData: chartData,
   };
